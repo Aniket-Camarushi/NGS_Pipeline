1929b8a7ea6f7878822a502a1ef28b3b13e28756
Aniket Camarushi <aniketcam@gmail.com>
2025-05-07 19:40:00 -0400

    Changes to pipeline

--- run_pipeline.sh
+++ run_pipeline.sh
@@ -1,22 +1,31 @@
-#!/bin/bash
-# Complete RNA-Seq pipeline: FASTQ -> Kallisto -> STAR -> GATK -> VCF
+# This script checks the quality of our fastq files and performs an alignment to the human cDNA transriptome reference with Kallisto.
+# To run this 'shell script' you will need to open your terminal and navigate to the directory where this script resides on your computer.
+# This should be the same directory where your fastq files and reference fasta files are found.
+# Change permissions on your computer so that you can run a shell script by typing: 'chmod u+x readMapping.sh' (without the quotes) at the terminal prompt
+# Then type './readMapping.sh' (without the quotes) at the prompt.
+# This will begin the process of running each line of code in the shell script.
+
 # Exit on error
 set -e
 
-echo "[$(date)] Starting pipeline" >> pipeline.log
-
-# 1. Quality Control with FastQC
+# First use fastqc to check the quality of our fastq files:
 echo "Running FastQC..."
 mkdir -p FastQC
 fastqc ./Data/*.gz -t 20 -o FastQC/
 
-
-# 2. Kallisto Quantification
+# Next, we want to build an index from our reference fasta file
+# I get my reference mammalian transciptome files from here: https://useast.ensembl.org/info/data/ftp/index.html
 echo "Building kallisto index..."
 mkdir -p Kallisto
 kallisto index -i ./Kallisto/Homo_sapiens.GRCh38.cdna.all_my.index ./Data/Homo_sapiens.GRCh38.cdna.all.fa
 
+# Now map reads to the indexed reference host transcriptome
+# Use as meany 'threads' as your machine will allow in order to speed up the read mapping process
+# Use 30-60 bootstraps to model technical variance for each sample
+# Note that we're also including the '&>' at the end of each line
+## this takes the information that would've been printed to our terminal & outputs this in a log file that is saved in your parent directory
 echo "Running kallisto quantifications..."
+
 # First the healthy subject (H5)
 kallisto quant -i ./Kallisto/Homo_sapiens.GRCh38.cdna.all_my.index  -o ./Kallisto/HS01 -t 26 --single -l 250 -s 30 ./Data/SRR8668755.fastq.gz &> ./Kallisto/HS01.log
 kallisto quant -i ./Kallisto/Homo_sapiens.GRCh38.cdna.all_my.index  -o ./Kallisto/HS02 -t 26 --single -l 250 -s 30 ./Data/SRR8668756.fastq.gz &> ./Kallisto/HS02.log
@@ -31,117 +40,10 @@ kallisto quant -i ./Kallisto/Homo_sapiens.GRCh38.cdna.all_my.index  -o ./Kallist
 kallisto quant -i ./Kallisto/Homo_sapiens.GRCh38.cdna.all_my.index  -o ./Kallisto/CL12 -t 26 --single -l 250 -s 30 ./Data/SRR8668773.fastq.gz &> ./Kallisto/CL12.log
 kallisto quant -i ./Kallisto/Homo_sapiens.GRCh38.cdna.all_my.index  -o ./Kallisto/CL13 -t 26 --single -l 250 -s 30 ./Data/SRR8668774.fastq.gz &> ./Kallisto/CL13.log
 
-
-# 3. STAR Alignment Preparation
-echo "Preparing STAR genome index..."
-mkdir -p STAR
-STAR --runThreadN 26 \
-     --runMode genomeGenerate \
-     --genomeDir ./STAR/GRCh38_index \
-     --genomeFastaFiles ./Data/Homo_sapiens.GRCh38.dna.primary_assembly.fa \
-     --sjdbGTFfile ./Data/Homo_sapiens.GRCh38.108.gtf \
-     --sjdbOverhang 100
-
-
-echo "Running STAR alignments..."
-
-# Define directories and parameters
-FASTQ_DIR="./Data"
-INDEX="./STAR/GRCh38_index"
-THREADS=26
-BAM_RAM_LIMIT=30000000000  # 30GB for BAM sorting
-
-# Process each FASTQ file
-for fastq in $FASTQ_DIR/*.fastq.gz; do
-    SAMPLE_NAME=$(basename ${fastq%.fastq.gz})
-    
-    echo "Processing sample: $SAMPLE_NAME"
-    
-    STAR --genomeDir $INDEX \
-         --readFilesIn $fastq \
-         --readFilesCommand zcat \
-         --outFileNamePrefix ./STAR/${SAMPLE_NAME}_ \
-         --runThreadN $THREADS \
-         --limitBAMsortRAM $BAM_RAM_LIMIT \
-         --outSAMtype BAM SortedByCoordinate \
-         --quantMode TranscriptomeSAM GeneCounts \
-         --outSAMattrRGline ID:${SAMPLE_NAME} SM:${SAMPLE_NAME} LB:lib1 PL:ILLUMINA PU:unit1 \
-         --outFilterType BySJout \
-         --outFilterMultimapNmax 20 \
-         --alignSJoverhangMin 8 \
-         --alignSJDBoverhangMin 1 \
-         --outFilterMismatchNmax 999 \
-         --outFilterMismatchNoverLmax 0.04 \
-         --alignIntronMin 20 \
-         --alignIntronMax 1000000 \
-         --alignMatesGapMax 1000000 \
-         --sjdbScore 1 \
-         --twopassMode Basic \
-         --outSAMattributes NH HI AS nM NM MD XS
-done
-
-
-# 5. Post-alignment processing
-echo "Processing BAM files..."
-for bam in ./STAR/*Aligned.sortedByCoord.out.bam; do
-    base=${bam%Aligned.sortedByCoord.out.bam}
-    picard MarkDuplicates \
-        I=$bam \
-        O=${base}_dedup.bam \
-        M=${base}_dedup_metrics.txt
-    samtools index ${base}_dedup.bam
-done
-
-
-# 6. Variant Calling with GATK
-echo "Running GATK variant calling..."
-mkdir -p GATK
-
-# First create a sequence dictionary for the reference
-picard CreateSequenceDictionary \
-    R=./Data/Homo_sapiens.GRCh38.dna.primary_assembly.fa \
-    O=./Data/Homo_sapiens.GRCh38.dna.primary_assembly.dict
-
-# Index the reference
-samtools faidx ./Data/Homo_sapiens.GRCh38.dna.primary_assembly.fa
-
-# Run HaplotypeCaller for each sample
-for bam in ./STAR/*_dedup.bam; do
-    base=$(basename $bam _dedup.bam)
-    gatk HaplotypeCaller \
-        -R ./Data/Homo_sapiens.GRCh38.dna.primary_assembly.fa \
-        -I $bam \
-        -O ./GATK/${base}.g.vcf.gz \
-        -ERC GVCF
-done
-
-# Combine GVCFs (FIXED: use a loop to include all samples)
-GVCFs=$(ls ./GATK/*.g.vcf.gz | tr '\n' ' ')  # Get all GVCFs
-gatk CombineGVCFs \
-    -R ./Data/Homo_sapiens.GRCh38.dna.primary_assembly.fa \
-    $(printf -- "--variant %s " $GVCFs)  # Dynamically add all samples
-    -O ./GATK/combined.g.vcf.gz
-
-# Joint genotype
-gatk GenotypeGVCFs \
-    -R ./Data/Homo_sapiens.GRCh38.dna.primary_assembly.fa \
-    -V ./GATK/combined.g.vcf.gz \
-    -O ./GATK/joint_variants.vcf.gz
-
-# 7. HARD FILTERING (instead of VQSR)
-echo "Applying hard filters..."
-gatk VariantFiltration \
-    -R ./Data/Homo_sapiens.GRCh38.dna.primary_assembly.fa \
-    -V ./GATK/joint_variants.vcf.gz \
-    --filter-expression "QD < 2.0 || FS > 60.0 || MQ < 40.0 || SOR > 3.0 || MQRankSum < -12.5 || ReadPosRankSum < -8.0" \
-    --filter-name "GATK_RNAseq_filter" \
-    -O ./GATK/filtered_variants.vcf.gz
-
-
-# 8. MultiQC summary
+# summarize fastqc and kallisto mapping results in a single summary html using MultiQC
 echo "Running MultiQC summary..."
-mkdir -p MultiQC
-multiqc . -o MultiQC/
+mkdir -p multiqc
+multiqc . -o multiqc/
 
-echo "Pipeline finished successfully!"
+echo "Finished"
 
